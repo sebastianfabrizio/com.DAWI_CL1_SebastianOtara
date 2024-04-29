9ecc9fa16a7c3263540e8aef72ef3a09109849db
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-04-29 14:32:50 -0500

    Primer commit

---
+++ src/main/java/com/cibertec/com/DAWI_CL1_SebastianOtara/Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.com.DAWI_CL1_SebastianOtara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/com/DAWI_CL1_SebastianOtara/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.com.DAWI_CL1_SebastianOtara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
